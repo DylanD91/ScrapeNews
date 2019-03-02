607ab9189bfe0626f32bf0e716e689b943179548
Dylan Dusenberry <dylandusenberry975@gmail.com>
2019-03-01 19:42:14 -0600

    updates

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
+const moment = require("moment");
 
 // Initialize Express
 const app = express();
@@ -10,3 +11,6 @@ app.use(express.static("public"));
 
 // Require all models
 const db = require("./models");
+
+// PORT
+const PORT = process.env.PORT || 3000;
