ed2da5609bf4da3766d5b266765a5d42ada529a1
Dylan Dusenberry <dylandusenberry975@gmail.com>
2019-03-02 19:07:34 -0600

    updates

--- models/note.js
+++ models/note.js
@@ -1,3 +1,27 @@
 // Mongoose and Schema
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+// Moment
+const moment = require('moment');
+
+// Library Schema for Comments
+const NoteSchema = new Schema({
+
+    name: {
+      type: String
+      // unique: true
+    },
+    note: {
+      type: String
+    },
+    dateCreated: {
+      type: Date,
+      default: Date.now
+    },
+});
+
+// Mongoose Method
+var Comment = mongoose.model('Note', NoteSchema);
+// Export
+module.exports = Note;
