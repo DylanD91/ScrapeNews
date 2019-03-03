191109e924a92aee2f7479a0b6545ba9dfb47217
Dylan Dusenberry <dylandusenberry975@gmail.com>
2019-03-02 19:03:59 -0600

    updates

--- models/article.js
+++ models/article.js
@@ -3,3 +3,27 @@ const mongoose = require('mongoose');
 
 // Schema
 const Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    summary: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    },
+    saved: {
+      type: Boolean,
+      default: false
+    },
+  });
+// Creating article Schema
+const Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
+module.exports = Article;
